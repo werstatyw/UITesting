b3f451d9c2710bbdfddbf3e16ef0d01316d94d06
Alex G <isaid.zx@gmail.com>
2018-05-23 10:31:34 +0300

    uitesting

---
+++ UITesting/Program.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace UITesting
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+			Console.ReadLine();
+        }
+    }
+}
---
+++ UITesting/Test.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using UITesting.Framework.Core;
+
+namespace UITesting
+{
+	[TestFixture()]
+    public class Test
+    {
+		private String baseURL;
+		private IWebDriver driver;
+        [SetUp]
+		public void SetUp()
+		{
+			baseURL = Configuration.Get("BaseURL");
+			ChromeOptions options = new ChromeOptions();
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			Driver.Add(Configuration.Get("Browser"), Path.GetFullPath("/home/wers4/Projects/UITesting/UITesting/chromedriver.exe"), capabilities);
+			driver = Driver.Current();
+			driver.Navigate().GoToUrl(baseURL);
+		}
+        [TearDown]
+        public void TearDown()
+		{
+			driver.Quit();	
+		}
+		[Test()]
+        public void Testcase()
+        {
+            driver.FindElement(By.Id("ss")).Click();
+			driver.FindElement(By.Id("ss")).Clear();
+			driver.FindElement(By.Id("ss")).SendKeys("London");
+			driver.FindElement(By.XPath("//table[@class='c2-month-table']//td[contains(@class. 'c2-day-today')]")).Click();
+            driver.FindElement(By.Id("group_adults")).Click();
+			new SelectElement(driver.FindElement(By.Id("group_adults"))).SelectByText("1");
+			driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+			driver.FindElement(By.Id("ss")).Click();
+        }
+    }
+}
